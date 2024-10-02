af19c5355c3367e5b939f395d6ccf3c93d09381f
nayandattabarua <nayandattabarua122221@gmail.com>
2024-10-02 18:30:26 +0600

    some change1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,36 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./index.html",
+//     // "./index.html",  
+//     // "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         'sans': ['ui-sans-serif', 'system-ui'],
+//         'serif': ['ui-serif', 'Georgia'],
+//         'mono': ['ui-monospace', 'SFMono-Regular'],
+//         'display': ['Oswald'],
+//         'body': ['"Open Sans"'],
+//       },
+//       gridTemplateColumns:{
+//         '70/30':'70%:30%',
+//       }
+//     },
+//   },
+//   plugins: [],
+
+// }
+
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./index.html",
-    // "./index.html",  
-    // "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/helper/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -17,11 +43,18 @@ export default {
       },
       gridTemplateColumns:{
         '70/30':'70%:30%',
-      }
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
   },
-  plugins: [],
-  build: {
-    outDir: 'dist', // Ensure Netlify points to the correct directory
-  },
-}
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *'); // Targets direct children
+      addVariant('child-hover', '& > *:hover'); // Targets direct children on hover
+    },
+  ],
+};
