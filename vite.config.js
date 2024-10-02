0e17b6aa377f5a5c3c317e0194b446e1ef881f1d
nayandattabarua <nayandattabarua122221@gmail.com>
2024-10-02 16:13:31 +0600

    vite comfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root:'src',
+  publicDir:'public',
+  
   plugins: [react()],
   server :{
     port:3000
