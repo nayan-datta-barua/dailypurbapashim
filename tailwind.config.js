f863191d0407ddb73c4a36fe11e320ab4099dc3d
nayandattabarua <nayandattabarua122221@gmail.com>
2024-10-02 07:26:59 +0600

    config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/components/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
     "./public/index.html",
     // "./index.html",
     // "./src/**/*.{js,ts,jsx,tsx}",
