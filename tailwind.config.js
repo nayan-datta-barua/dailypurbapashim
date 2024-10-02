17740d97afcbfa2e582158eeb44e875b0a6dfa25
nayandattabarua <nayandattabarua122221@gmail.com>
2024-10-02 15:49:58 +0600

    git config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./index.html",
-    // "./index.html",
+    // "./index.html",  
     // "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
